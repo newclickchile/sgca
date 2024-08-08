2609e22031a903d786ea96f39a2221b300390a88
Héctor Vera <healvey@gmail.com>
2024-08-08 13:31:01 -0400

    feat: remove unnecessary code

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,10 +3,9 @@ import type { ChildrenType, Direction } from '@core/types'
 
 // Context Imports
 import { NextAuthProvider } from '@/contexts/nextAuthProvider'
-import { VerticalNavProvider } from '@menu/contexts/verticalNavContext'
-import { SettingsProvider } from '@core/contexts/settingsContext'
 import ThemeProvider from '@components/theme'
-import ReduxProvider from '@/redux-store/ReduxProvider'
+import { SettingsProvider } from '@core/contexts/settingsContext'
+import { VerticalNavProvider } from '@menu/contexts/verticalNavContext'
 
 // Styled Component Imports
 import AppReactToastify from '@/libs/styles/AppReactToastify'
@@ -33,7 +32,7 @@ const Providers = (props: Props) => {
       <VerticalNavProvider>
         <SettingsProvider settingsCookie={settingsCookie} mode={mode} demoName={demoName}>
           <ThemeProvider direction={direction} systemMode={systemMode}>
-            <ReduxProvider>{children}</ReduxProvider>
+            {children}
             <AppReactToastify direction={direction} hideProgressBar />
           </ThemeProvider>
         </SettingsProvider>
